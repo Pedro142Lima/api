c1d831cc6783e679757557a2d78d2a307e7f74a0
Pedro <pedroeinviller142@gmail.com>
2024-08-02 17:52:35 -0300

    Api

---
+++ api.js
@@ -0,0 +1,18 @@
+import express from 'express'
+const servidor = express()
+
+
+servidor.listen(5001, () => console.log('A api SUBIU::::'));
+
+servidor.get('/somar/:n1/:n2', (req,resp) => {
+    let n1 = Number(req.params.n1)
+    let n2 = Number(req.params.n2)
+
+    let soma = n1 + n2
+
+    resp.send({
+        soma
+    })
+    })
+
+
