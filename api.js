0e74db6c52d409b4b89cd9b675000e596bb2d90b
Pedro <pedroeinviller142@gmail.com>
2024-08-02 18:06:15 -0300

    subiu

--- api.js
+++ api.js
@@ -16,3 +16,17 @@ servidor.get('/somar/:n1/:n2', (req,resp) => {
     })
 
 
+
+    servidor.get ('/multi/:n1/:n2', (req,resp) => {
+        let n1 = Number(req.params.n1)
+        let n2 = Number (req.params.n2)
+
+        let n3 = n1 * n2
+        
+
+        resp.send ({
+            n3
+        })
+
+    })
+
